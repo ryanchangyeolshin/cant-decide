6e5cc1690f061f85e644e8f33f9e6ee0938992a5
Ryan Shin <changyeolshin@gmail.com>
2021-02-26 08:19:51 +0900

    Initialized create-react-app, imported material-ui, and rendered the NavMenuBar.tsx component.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavMenuBar from './components/NavMenuBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavMenuBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavMenuBar.tsx
@@ -0,0 +1,37 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { Menu as MenuIcon } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    toolBar: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between"
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+  }),
+);
+
+export default function NavMenuBar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolBar}>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
