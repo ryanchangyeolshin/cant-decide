9933d92ab56f5f30ebae4c315f95d88d6ae1b4f1
Ryan Shin <changyeolshin@gmail.com>
2021-03-03 11:39:07 +0900

    Got rid of unused useContext.

--- src/tests/ChoiceCardContainer.test.tsx
+++ src/tests/ChoiceCardContainer.test.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { render, getByTestId, fireEvent } from '@testing-library/react';
 import ChoiceCardContainer from '../components/ChoiceCardContainer';
 import ChoiceCardContainerProps from '../types/ChoiceCardContainerProps';
